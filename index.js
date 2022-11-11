3b87395bfa581b1ab8c36d777490a269a563c2f1
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-11-11 23:45:32 +0300

    test10

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 const express = require("express");
+const http = require("http");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = 5000;
+const server = http.createServer(app);
 // import the routers
-
 const loginRouter = require("./routes/login");
 const messageRouter = require("./routes/message");
 const conversationRouter = require("./routes/conversation");
 
+mongoose.connect("mongodb://localhost:27017/Chat_app").then(
+  () => {
+    console.log("database connected");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 // endpoints Router
-
 app.use("/user", loginRouter);
 app.use("/message", messageRouter);
 app.use("/conversation", conversationRouter);
-app.get("/", function (req, res) {
-  // const protocol = req.protocol;
-  // const host = req.hostname;
-  // const url = req.originalUrl;
-  // const port = process.env.PORT || PORT;
 
-  // const fullUrl = `${protocol}://${host}:${port}${url}`;
-
-  // const responseString = `Full URL is: ${fullUrl}`;
-  res.send("hello world");
+server.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
 });
-const url = app.listen(PORT, () => {
-  console.log(`App listening at port :${PORT}`);
-});
-// require("./socket");
+require("./socket");
