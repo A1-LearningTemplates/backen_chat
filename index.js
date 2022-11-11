9c9c9533e4dd5aec2002d481935843dda62c8739
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-11-11 20:22:07 +0300

    update server

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const http = require("http");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = 5000;
-const server = http.createServer(app);
 // import the routers
 require("./socket");
 const loginRouter = require("./routes/login");
@@ -26,10 +24,21 @@ mongoose
     }
   );
 // endpoints Router
+
 app.use("/user", loginRouter);
 app.use("/message", messageRouter);
 app.use("/conversation", conversationRouter);
+app.get("*", function (req, res) {
+  const protocol = req.protocol;
+  const host = req.hostname;
+  const url = req.originalUrl;
+  const port = process.env.PORT || PORT;
 
-server.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`);
+  const fullUrl = `${protocol}://${host}:${port}${url}`;
+
+  const responseString = `Full URL is: ${fullUrl}`;
+  res.send(responseString);
+});
+const url = app.listen(PORT, () => {
+  console.log(`App listening at port :${PORT}`);
 });
