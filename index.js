b73e10de7f5e692fd5009227e7cbcc9175b445d7
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-11-13 12:59:18 +0300

    fix db connection

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 app.use(cors());
+require("./models/db");
 const PORT = process.env.PORT || 5000;
 // import the routers
 const loginRouter = require("./routes/login");
 const messageRouter = require("./routes/message");
 const conversationRouter = require("./routes/conversation");
 
-mongoose.connect("mongodb://localhost:27017/Chat_app").then(
-  () => {
-    console.log("database connected");
-  },
-  (err) => {
-    console.log(err);
-  }
-);
 // endpoints Router
 app.use("/user", loginRouter);
 app.use("/message", messageRouter);
