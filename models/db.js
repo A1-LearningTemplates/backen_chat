4896603b181d48385704d9b57377223f60c26f43
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-11-13 16:32:35 +0300

    db

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://anas:gZ1HNZAztk9XdNvw@cluster0.zc8ky.mongodb.net/?retryWrites=true&w=majority").then(
+mongoose.connect("mongodb+srv://anas:JCd9tDowdCLNMbE0@cluster0.zc8ky.mongodb.net/?retryWrites=true&w=majority").then(
   () => {
     console.log("database connected");
   },
