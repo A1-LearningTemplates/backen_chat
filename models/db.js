6ab7a13bbd504a4f3733760b999d8e4c49f96a43
AnasAlkhamis <anas.alkhamnis@gmail.com>
2022-11-13 16:21:52 +0300

    db connection

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Chat_app").then(
+mongoose.connect("mongodb+srv://anas:gZ1HNZAztk9XdNvw@cluster0.zc8ky.mongodb.net/?retryWrites=true&w=majority").then(
   () => {
     console.log("database connected");
   },
